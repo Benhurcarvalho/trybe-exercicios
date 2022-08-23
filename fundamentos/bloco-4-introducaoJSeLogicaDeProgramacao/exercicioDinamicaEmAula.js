15a916a0299b975a4c8b819326311a0e09db1f29
Ben-Hur <benhurcarvalho2@gmail.com>
2022-08-23 16:03:33 -0300

    first exercise

---
+++ fundamentos/bloco-4-introducaoJSeLogicaDeProgramacao/exercicioDinamicaEmAula.js
@@ -0,0 +1,10 @@
+// 1 - 
+// Faça um algoritmo que calcule a soma de 1 a 50 usando a estrutura "for" e retorne no formato:
+//A soma total de 1 a 50 é:
+
+let soma = 0;
+for (let index = 0; index <50; index+=1){
+soma += index;
+}
+console.log(soma);
+
