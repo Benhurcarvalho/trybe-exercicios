1b230a4f36454d8cb185d4f16f1b59e0360a2ae8
Ben-Hur <benhurcarvalho2@gmail.com>
2022-09-05 12:36:43 -0300

    exercícios 5.1.1

---
+++ fundamentos/Bloco-05-IntrJavaScriptDomProjetos/5-1-1-JavaScriptDomEseletores.js
@@ -0,0 +1,6 @@
+let pilotosDeF1 = document.getElementsByClassName("pilotos-f1-atual");
+
+    for (let i = 0; i < pilotosDeF1.length; i +=1) {
+        pilotosDeF1[i].innerText = "Lewis Hamilton"
+    }
+    
