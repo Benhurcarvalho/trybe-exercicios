bab337a7f312431d543656be2d474d4ba905f0d0
Ben-Hur <benhurcarvalho2@gmail.com>
2022-10-06 16:19:44 -0300

    front-callback

--- front-end/Seção: JavaScript e Testes Assíncronos/forntDia1.js
+++ front-end/Seção: JavaScript e Testes Assíncronos/forntDia1.js
@@ -15,7 +15,25 @@ const getUser = (callback) => {
 };
 
 // Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
-console.log(getUser(userFullName));
+// console.log(getUser(userFullName));
 
 // Saída:
 // Olá! Meu nome é Ivan Ivanovich
+
+// callbacks-para-fixar-01.js
+
+// const userFullName = ({ firstName, lastName }) => `Olá! Meu nome é ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} é ${nationality}`;
+
+const getUser2 = (callback) => {
+  const user = {
+    firstName: 'Ivan',
+    lastName: 'Ivanovich',
+    nationality: 'Russo',
+  };
+  // Insira o retorno da função `getUser`
+  return callback(user);
+};
+
+console.log(getUser2(userFullName)); // Retorno esperado: "Olá! Meu nome é Ivan Ivanovich"
+console.log(getUser2(userNationality)); // Retorno esperado: "Ivan é Russo"
