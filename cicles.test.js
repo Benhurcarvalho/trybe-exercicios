8d3d5ced06110aaa4de63d777d6fc2aeac828a9c
Ben-Hur <benhurcarvalho2@gmail.com>
2022-10-07 10:40:00 -0300

    teste com beforEach

--- cicles.test.js
+++ cicles.test.js
@@ -7,3 +7,30 @@ const addCity = (city) => {
 const removeCity = (city) => {
   cities = cities.filter((eachCity) => eachCity !== city);
 };
+
+beforeEach(() => {
+    cities = [...cities, 'Pindamonhangaba'];
+  });
+
+test('Testa a função addCity utilizando o beforeEach', () => {
+    expect.assertions(5);
+    addCity('Campinas');
+    addCity('Goiania');
+    addCity('Recife');
+    expect(cities).toHaveLength(4);
+    expect(cities).toContain('Campinas');
+    expect(cities).toContain('Goiania');
+    expect(cities).toContain('Recife');
+    expect(cities).toContain('Pindamonhangaba');
+  });
+  
+  test('Testa a função removeCity utilizando o beforeEach', () => {
+    expect.assertions(5);
+    removeCity('Campinas');
+    expect(cities).toHaveLength(4);
+    expect(cities).not.toContain('Campinas');
+    expect(cities).toContain('Goiania');
+    expect(cities).toContain('Recife');
+    expect(cities).toContain('Pindamonhangaba');
+    console.log(cities);
+  });
