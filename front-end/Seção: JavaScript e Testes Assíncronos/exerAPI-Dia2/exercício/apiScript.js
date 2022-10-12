12a171dddf8c3c43b605b3835905ffac6cb3f700
Ben-Hur <benhurcarvalho2@gmail.com>
2022-10-12 20:28:37 -0300

    .

--- front-end/Seção: JavaScript e Testes Assíncronos/exerAPI-Dia2/exercício/apiScript.js
+++ front-end/Seção: JavaScript e Testes Assíncronos/exerAPI-Dia2/exercício/apiScript.js
@@ -1,8 +1,42 @@
 // apiScript.js
+const fetch = require('node-fetch');
+
+function append(piada) {
+    const section = document.getElementById("piadaAleatoria");
+  
+    const divCard = document.createElement("div");
+    const divBody = document.createElement("div");
+    const cardTitle = document.createElement("h2");
+  
+    divCard.classList.add("card");
+    divBody.classList.add("card-body");
+    cardTitle.classList.add("card-title");
+  
+    cardTitle.innerHTML = piada;
+  
+    divBody.appendChild(cardTitle);
+    divCard.appendChild(divBody);
+    section.appendChild(divCard);
+  }
+
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
   // Adicionar lógica aqui!
-};
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' },
+  };
 
-window.onload = () => fetchJoke();
+  fetch(API_URL, myObject)
+    .then((response) => response.json())
+    .then((data) => {
+        const piada = data.joke;
+        append(piada)
+        console.log(piada)
+    })
+    // .catch(() => console.log('not funny joke')) 
+}
+
+// window.onload = () => 
+fetchJoke();
