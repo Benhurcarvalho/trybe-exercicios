c0690bdd179e947a430afd05f2459cb05becec52
Ben-Hur <benhurcarvalho2@gmail.com>
2022-10-13 21:49:09 -0300

    joke

--- front-end/Seção: JavaScript e Testes Assíncronos/exerAPI-Dia2/exercício/apiScript.js
+++ front-end/Seção: JavaScript e Testes Assíncronos/exerAPI-Dia2/exercício/apiScript.js
@@ -1,42 +1,43 @@
-// apiScript.js
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-function append(piada) {
-    const section = document.getElementById("piadaAleatoria");
+// function append(piada) {
+//     const section = document.getElementById("piadaAleatoria");
   
-    const divCard = document.createElement("div");
-    const divBody = document.createElement("div");
-    const cardTitle = document.createElement("h2");
+//     const divCard = document.createElement("div");
+//     const divBody = document.createElement("div");
+//     const cardTitle = document.createElement("h2");
   
-    divCard.classList.add("card");
-    divBody.classList.add("card-body");
-    cardTitle.classList.add("card-title");
+//     divCard.classList.add("card");
+//     divBody.classList.add("card-body");
+//     cardTitle.classList.add("card-title");
   
-    cardTitle.innerHTML = piada;
+//     cardTitle.innerHTML = piada;
   
-    divBody.appendChild(cardTitle);
-    divCard.appendChild(divBody);
-    section.appendChild(divCard);
-  }
+//     divBody.appendChild(cardTitle);
+//     divCard.appendChild(divBody);
+//     section.appendChild(divCard);
+//   }
 
 const API_URL = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = () => {
+const fetchJoke = async () => {
   // Adicionar lógica aqui!
   const myObject = {
     method: 'GET',
     headers: { 'Accept': 'application/json' },
   };
 
-  fetch(API_URL, myObject)
+   return fetch(API_URL, myObject)
     .then((response) => response.json())
-    .then((data) => {
-        const piada = data.joke;
-        append(piada)
-        console.log(piada)
-    })
-    // .catch(() => console.log('not funny joke')) 
+    .then((data) => data.joke)
+    .catch(() => console.log('not funny joke'));
+
 }
 
-// window.onload = () => 
-fetchJoke();
+// window.onload = () => fetchJoke().then((data) => console.log(data));
+
+fetchJoke().then((data) => {
+  const jokeDom = document.createElement('p');
+  jokeDom.innerText = data
+  document.getElementById('piadaAleatoria').appendChild(jokeDom);
+  });
