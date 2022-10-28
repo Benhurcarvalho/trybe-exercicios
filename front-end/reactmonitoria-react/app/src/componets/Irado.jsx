0518aae04af2978c272d00a2ed30e945985711fc
Ben-Hur <benhurcarvalho2@gmail.com>
2022-10-27 21:42:17 -0300

    componente xablau

---
+++ front-end/reactmonitoria-react/app/src/componets/Irado.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Irado extends React.Component { 
+ render() {
+
+    const { xablau } = this.props;
+
+     return ( 
+    <div>
+        { xablau }
+        {/* <h1> Olá povo Irado</h1> */}
+    </div>
+    )
+  }      
+}
+
+export default Irado 
