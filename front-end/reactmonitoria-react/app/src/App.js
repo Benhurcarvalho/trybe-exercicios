f9b5d508b8a157e0cfaebf7c938fbbc611fa81f6
Ben-Hur <benhurcarvalho2@gmail.com>
2022-11-02 18:08:19 -0300

    aplica logo ao App

--- front-end/reactmonitoria-react/app/src/App.js
+++ front-end/reactmonitoria-react/app/src/App.js
@@ -1,13 +1,19 @@
-import PasswordInput from './componets/PasswordInput';
-import PaletaCores from './componets/PaletaCores';
+// import PasswordInput from './componets/PasswordInput';
+// import PaletaCores from './componets/PaletaCores';
+import logo from './trybe_logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <main className='xablau'>
-      <PaletaCores />
-      <PasswordInput />
+    <div>
+      <header>
+        <img src={ logo } alt='logo da trybe' />
+      </header>
+    <main className='app'>
+      {/* <PaletaCores /> */}
+      {/* <PasswordInput /> */}
     </main>
+    </div>
   );
 }
 
